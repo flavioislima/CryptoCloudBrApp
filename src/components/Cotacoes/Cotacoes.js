25763d52fdedc73cdc2954e0245597f20b47a6c0
Flavio F Lima <flavioislima@gmail.com>
2017-12-03 23:27:14 -0300

    Adding Refresh Control (not working)

--- src/components/Cotacoes/Cotacoes.js
+++ src/components/Cotacoes/Cotacoes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View, ScrollView, RefreshControl } from 'react-native';
 import axios from 'axios';
 import Valores from './Valores';
 import Rodape from '../Rodape/rodape';
@@ -7,8 +7,9 @@ import Rodape from '../Rodape/rodape';
 export default class Cotacoes extends Component<{}> {
   constructor(props) {
     super(props);
-    this.state = { listaMoedas: [] };
+    this.state = { listaMoedas: [], refreshing: false, };
   }
+
 
   componentWillMount() {
     axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=BRL&limit=15')
@@ -16,10 +17,26 @@ export default class Cotacoes extends Component<{}> {
     .catch(() => { console.log('Erro ao recuperar os dados'); });
   }
 
+
+  _onRefresh() {
+    this.setState({ refreshing: true });
+    axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=BRL&limit=15')
+    .then(response => { this.setState({ listaMoedas: response.data, refreshing: false }); })
+    .catch(() => { console.log('Erro ao recuperar os dados'); });
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <ScrollView style={{ flex: 8 }}>
+        <ScrollView
+          style={{ flex: 8 }}
+           refreshControl={
+                            <RefreshControl
+                              refreshing={this.state.refreshing}
+                              onRefresh={this._onRefresh.bind(this)}
+                            />
+                            }
+        >
           <Text style={{ textAlign: 'center', fontSize: 20 }}>Cotações das Principais Moedas</Text>
           { this.state.listaMoedas.map(item => (<Valores key={item.symbol} item={item} />))}
         </ScrollView>
